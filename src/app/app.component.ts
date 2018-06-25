a11be40ee7c00fd6881d81dfd073e8436ac9c094
Daniela Bosch <danielarbosch@gmail.com>
2018-06-25 17:30:01 -0300

    full page scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  @HostListener('window:wheel', ['$event'])
+  public onWheelEvent(event: Event) {
+    event.preventDefault();
+    var wheel = event.deltaY;
+    var offset = window.pageYOffset;
+    var size = window.innerHeight;
+
+    // prevent bad offset triggered by multiple wheel event
+    var mod_offset = offset - (offset % size);
+
+    if(wheel > 0) {
+      // Scroll down
+      window.scrollTo({
+        top: mod_offset + size,
+        behavior: "smooth"
+      });
+    } else {
+      // Scroll up
+      window.scrollTo({
+        top: mod_offset - size,
+        behavior: "smooth"
+      }); 
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  public onKeyEvent(event: Event) {
+    event.preventDefault();
+    var offset = window.pageYOffset;
+    var size = window.innerHeight;
+
+    // prevent bad offset triggered by multiple keypress event
+    var mod_offset = offset - (offset % size);
+
+    if(event.key === "ArrowDown") {
+      // Scroll down
+      window.scrollTo({
+        top: mod_offset + size,
+        behavior: "smooth"
+      });
+    } else if(event.key === "ArrowUp") {
+      // Scroll up
+      window.scrollTo({
+        top: mod_offset - size,
+        behavior: "smooth"
+      }); 
+    }
+  }
 }
